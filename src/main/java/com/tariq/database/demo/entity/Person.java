0302f20bf4b52344613fa3e4c565371c57b40aed
Tariq Sarfaraz <tariqsarfaraz7@gmail.com>
2022-07-16 16:21:23 +0530

    added

---
+++ src/main/java/com/tariq/database/demo/entity/Person.java
@@ -0,0 +1,28 @@
+package com.tariq.database.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private int id;
+    private String name;
+    private String location;
+    private Date birthDate;
+
+    @Override
+    public String toString() {
+        return "\nPerson{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
