f9834a32d10a9841de21f0e8b5670dc01c95fe26
Tariq Sarfaraz <tariqsarfaraz7@gmail.com>
2022-07-16 21:01:21 +0530

    modified

--- src/main/java/com/tariq/database/demo/DemoApplication.java
+++ src/main/java/com/tariq/database/demo/DemoApplication.java
@@ -9,19 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @Autowired
-    PersonJpaDao dao;
-
+public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        logger.info(" Person with Id - 10001 -> {}", dao.findById(10001));
-    }
 }
